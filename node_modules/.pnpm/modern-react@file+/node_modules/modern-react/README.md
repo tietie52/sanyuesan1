3aa680fc44a09cf4a1d0ad51696a6a93d2b548ab
yyqx <2325469508@qq.com>
2025-03-31 00:08:35 +0800

    陈婷婷新添代码

---
+++ node_modules/.pnpm/modern-react@file+/node_modules/modern-react/README.md
@@ -0,0 +1,18 @@
+# ForSchoolReact
+
+## 先决条件   
+1. 安装nodejs
+2. 安装pnpm ：  npm install -g pnpm  
+
+
+## 启动和运行
+pnpm  install  
+pnpm  run dev  
+
+
+## github基础操作  == 正式上课时再使用
+查看状态： git status  
+添加当前代码到笔记本电脑的本地仓库：  git add .  
+提交一个新版本: git commit -a -m 新版本号比如230113  
+提交本地版本到远程仓库： git push  
+更新本地的代码： git pull  
---
+++ node_modules/modern-react/src/Chengtingting/Yyqx.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+
+
+function App(){
+
+
+
+const onButtonClick=()=>{
+alert('点击了按钮')
+
+}
+
+
+
+    return(
+    <div>
+          <div className="bg-red-300 space-x-4 p-4 shadow-md font-semibold w-300 text-red-700">
+              <a href="/img/one.png ">
+                  <button>易</button>
+                  </a >
+                  </div>
+             
+  
+          <div className="bg-red-300 space-x-4 p-4 shadow-md font-semibold w-300 text-red-700">
+              <a href="/img/two.png">
+                  <button>烊</button>
+                  </a >
+                  </div>
+  
+          <div className="bg-red-300 space-x-4 p-4 shadow-md font-semibold w-300 text-red-700">
+              <a href="/img/three.png">
+                   <button>千</button>
+                       </a >
+                      </div>
+
+            <div className="bg-red-300 space-x-4 p-4 shadow-md font-semibold w-300 text-red-700">
+              <a href="/img/four.png">
+                   <button>玺</button>
+                       </a >
+                      </div>
+                      
+                      
+  </div>
+                     
+      )
+  }
+  
+  export default App;
